2b4a880924280b1b2a990ac7ee797d9ce4e62629
TKDGLabs <contact@tkdglabs.com>
2026-02-25 03:37:00 +0900

    wtf

--- components/SplineHero.tsx
+++ components/SplineHero.tsx
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function SplineHero() {
   return (
-    <div className="absolute inset-0 -z-10 hidden md:block pointer-events-none">
+    <div className="w-full h-[500px]">
       <Spline scene="https://prod.spline.design/zb9htpW08g2kO3Lk/scene.splinecode" />
     </div>
   );
