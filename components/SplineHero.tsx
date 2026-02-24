de0927f7f1f5860651478522da38e2e12e5ddfda
TKDGLabs <contact@tkdglabs.com>
2026-02-25 05:00:16 +0900

    update

--- components/SplineHero.tsx
+++ components/SplineHero.tsx
@@ -8,8 +8,13 @@ export default function SplineHero() {
       className="pointer-events-none absolute inset-0 z-0 hidden overflow-hidden md:block"
       aria-hidden="true"
     >
-      <div className="absolute inset-0 [&>*]:h-full [&>*]:w-full">
-        <Spline scene="https://prod.spline.design/zb9htpW08g2kO3Lk/scene.splinecode" />
+      {/* centered frame + side gutters to avoid left-heavy composition */}
+      <div className="absolute inset-y-0 left-1/2 w-full max-w-[1440px] -translate-x-1/2 px-12 lg:px-24">
+        <div className="relative h-full w-full overflow-hidden">
+          <div className="absolute inset-0 translate-x-[10%] scale-[1.08]">
+            <Spline scene="https://prod.spline.design/zb9htpW08g2kO3Lk/scene.splinecode" />
+          </div>
+        </div>
       </div>
     </div>
   );
