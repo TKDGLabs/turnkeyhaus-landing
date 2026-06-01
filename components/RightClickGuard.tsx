595ac30523308c5950026f66d9df93aadcf891da
TKDGLabs <contact@tkdglabs.com>
2026-06-01 13:55:23 +0900

    Update RightClickGuard.tsx

--- components/RightClickGuard.tsx
+++ components/RightClickGuard.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
-export default function RightClickGuard() {
+export default function RightClickGuard(): React.ReactNode {
   useEffect(() => {
     const handleContextMenu = (event: MouseEvent) => {
       event.preventDefault();
     };
-
+    
     document.addEventListener("contextmenu", handleContextMenu);
-
+    
     return () => {
       document.removeEventListener("contextmenu", handleContextMenu);
     };
